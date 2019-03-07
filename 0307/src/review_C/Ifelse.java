810301f5c3267fc1799e20367095375c8d13e631
jogyeongho <48301636+jogyeongho@users.noreply.github.com>
2019-03-07 10:38:26 +0900

    c와 유사한java문법 맛보기
    맞다 For문WHile문도 체험해봐야지

---
+++ 0307/src/review_C/Ifelse.java
@@ -0,0 +1,12 @@
+package review_C;
+
+public class Ifelse {
+public static void main(String[] args) {
+	int x =5;
+	if(x>=5) {
+		System.out.println("x는5이상입니다.");
+	}else {
+		System.out.println("x는5미만입니다.");
+	}
+}
+}
